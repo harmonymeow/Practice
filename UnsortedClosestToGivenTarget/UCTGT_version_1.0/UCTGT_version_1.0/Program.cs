2f5081bb27f1aa3ac592b87b1d0647d20eccb139
Charles Changmin Han <name.cmh@gmail.com>
2013-06-05 10:05:31 -0700

    added givenList

--- UnsortedClosestToGivenTarget/UCTGT_version_1.0/UCTGT_version_1.0/Program.cs
+++ UnsortedClosestToGivenTarget/UCTGT_version_1.0/UCTGT_version_1.0/Program.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Given an unsorted integer list, find the closest number to a given target, 
+ * where closest means smaller than target with smallest difference. What 
+ * the best running time and why
+*/
 namespace UCTGT_version_1._0
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			List<int> givenList;
+			int givenTarget;
+
+		}
+
+		static int findClostest(List<int> givenList, int givenTarget)
+		{
+
 		}
 	}
 }

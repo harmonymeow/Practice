85de3952b875c645ba5fc916d141f19a48471d5f
Charles Changmin Han <name.cmh@gmail.com>
2013-06-06 02:33:20 -0700

    #6 completed

--- CharsCorrespondingProbability/CCP_version_1.0/CCP_version_1.0/Program.cs
+++ CharsCorrespondingProbability/CCP_version_1.0/CCP_version_1.0/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/* Given a list of chars and corresponding probability, we want a random method so that the 
+ * probability of producing a specific char is exactly same as in the probability list.
+*/
 namespace CCP_version_1._0
 {
 	class Program

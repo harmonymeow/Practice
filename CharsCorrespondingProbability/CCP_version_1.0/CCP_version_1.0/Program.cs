6e7cb4fb0c3e4bacccf0dcff329913a6939cb815
Charles Changmin Han <name.cmh@gmail.com>
2013-06-06 03:38:58 -0700

    checking in

--- CharsCorrespondingProbability/CCP_version_1.0/CCP_version_1.0/Program.cs
+++ CharsCorrespondingProbability/CCP_version_1.0/CCP_version_1.0/Program.cs
@@ -12,7 +12,41 @@ namespace CCP_version_1._0
 	{
 		static void Main(string[] args)
 		{
+			Dictionary<char, int> chars = new Dictionary<char, int>();
+			chars.Add('a', 10);
+			chars.Add('b', 20);
+			chars.Add('c', 30);
+			chars.Add('d', 40);
+			chars.Add('e', 50);
+			int[] ranges = new int[chars.Count+1];
+			ranges[0] = 0;
+			int i = 1;
+			int total = 0;
+			Dictionary<char, int> charsRanges = new Dictionary<char, int>();
+			foreach (var pair in chars)
+			{
+				total = total + pair.Value;
+				ranges[i] = total;
+				i++;
+				charsRanges.Add(pair.Key, total);
+			}
 
+
+			Console.ReadLine();			
 		}
+		
+		static char random(Dictionary<char, int> charsRanges, int[] ranges, int total)
+		{
+			Random r = new Random();
+			int n = r.Next(total);
+			return 'a';
+		}
+
+		static int BinarySearch(int[] ranges, int target)
+		{
+			
+			return 0;
+		}
+
 	}
 }

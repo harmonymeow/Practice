4bc4e7e53c65d497c2445209207be7852f0d327b
Charles Changmin Han <name.cmh@gmail.com>
2013-06-06 02:33:41 -0700

    source code for #6 updated

--- UnsortedClosestToGivenTarget/UCTGT_version_1.0/UCTGT_version_1.0/Program.cs
+++ UnsortedClosestToGivenTarget/UCTGT_version_1.0/UCTGT_version_1.0/Program.cs
@@ -14,14 +14,42 @@ namespace UCTGT_version_1._0
 	{
 		static void Main(string[] args)
 		{
-			List<int> givenList;
-			int givenTarget;
+			List<int> givenList = new List<int>();
+			givenList.Add(40);
+			givenList.Add(80);
+			givenList.Add(5);
+			givenList.Add(20);
+			givenList.Add(99);
+			givenList.Add(65);
+			givenList.Add(10);
 
+			// Test Cases
+			Console.WriteLine("Try: " + findClostest(givenList,0) + " Should be: -1");
+			Console.WriteLine("Try: " + findClostest(givenList, 50) + " Should be: 40");
+			Console.WriteLine("Try: " + findClostest(givenList, 39) + " Should be: 20");
+			Console.WriteLine("Try: " + findClostest(givenList, 99) + " Should be: 99");
+
+			Console.ReadLine();
 		}
 
-		static int findClostest(List<int> givenList, int givenTarget)
+		// Linear Search, Runtime Complexity of O(n)
+		static int findClostest(List<int> givenList, int target)
 		{
-			return 0;
+			int closest = -1;
+			int dif = target - closest;
+			foreach (int compare in givenList)
+			{
+				// Assuming that closest means smaller than or equal to the target value
+				if (target >= compare)
+				{
+					if (target - compare < dif)
+					{
+						closest = compare;
+						dif = target - closest;
+					}
+				}
+			}
+			return closest;
 		}
 	}
 }

3f76ab80d389cbcacd06736ab68d5bd2358e3bf0
Charles Changmin Han <name.cmh@gmail.com>
2013-06-24 18:09:40 -0700

    Solution 1

---
+++ RemoveEverySecondUntilOneLeft/RemoveEverySecondUntilOneLeft/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveEverySecondUntilOneLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 20;
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(i + " " + findRemaining(i));
+            }
+            Console.ReadKey();
+        }
+
+        static int findRemaining(int n)
+        {
+            double loged = (int)(Math.Log((double)n) / Math.Log(2.0));
+            int powered = (int)Math.Pow(2.0, loged);
+            return 1 + (n - powered) * 2;
+        }
+    }
+}

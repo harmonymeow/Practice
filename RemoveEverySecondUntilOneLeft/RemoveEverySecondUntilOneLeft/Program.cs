24d90fd8e2aada42ca33e6e9bd65b44b80f78b5c
Charles Changmin Han <name.cmh@gmail.com>
2013-06-24 22:01:54 -0700

    Added another approach (pseudocode)

--- RemoveEverySecondUntilOneLeft/RemoveEverySecondUntilOneLeft/Program.cs
+++ RemoveEverySecondUntilOneLeft/RemoveEverySecondUntilOneLeft/Program.cs
@@ -10,19 +10,26 @@ namespace RemoveEverySecondUntilOneLeft
     {
         static void Main(string[] args)
         {
-            int length = 20;
+            int length = 50;
             for (int i = 1; i <= length; i++)
             {
-                Console.WriteLine(i + " " + findRemaining(i));
+                Console.WriteLine(i + " " + findRemaining1(i));
             }
             Console.ReadKey();
         }
 
-        static int findRemaining(int n)
+        static int findRemaining1(int n)
         {
             double loged = (int)(Math.Log((double)n) / Math.Log(2.0));
             int powered = (int)Math.Pow(2.0, loged);
             return 1 + (n - powered) * 2;
+        }
+
+        static int findRemaining2(int n)
+        {
+            //Set leftmost 1 in binary number to 0
+            //multiply the result by 2 and add 1
+            return 1;
         }
     }
 }

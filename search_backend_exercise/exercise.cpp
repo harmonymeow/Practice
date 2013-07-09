80c1515f21a6d781a381f8e26a944b8e19d30745
OminStyle <name.cmh@gmail.com>
2013-07-09 00:12:40 -0700

    added deviantART Interview Question

---
+++ search_backend_exercise/exercise.cpp
@@ -0,0 +1,47 @@
+template <class T>
+class SortedList
+{
+public:
+    void Push(T value)
+    {
+        Node ** where = &m_head;
+        while (*where && (*where)->value < value) {
+            where = &((*where)->next);
+        }
+        *where = new Node(value,*where);
+    }
+
+    T Pop()
+    {
+        Node* pResult = m_head;
+        m_head = m_head->next;
+        T result = pResult->value;
+        free(pResult);
+        return result;
+    }
+
+    int Size()
+    {
+        Node * p = m_head;
+        int size = 0;
+        while (p)
+            ++size;
+            p = p->next;
+        return size;
+    }
+
+private:
+    class Node
+    {
+        Node(T value_, Node* next_)
+        {
+            value = value_;
+            next = next_;
+        }
+        T value;
+        Node* next;
+    };
+
+    Node* m_head;
+};
+

090e2d453f3785cdaf07eb80e9ed12161fded39f
OminStyle <name.cmh@gmail.com>
2013-07-10 01:12:52 -0700

    created solution files

---
+++ search_backend_exercise/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "exercise.cpp"
+using namespace std;
+
+int main ()
+{
+  int a = 100;
+  int *b = &a;
+  int c* = b
+  cout << a << endl << b << endl << c ;
+  return 0;
+}

f9abd52f5c342a0854a37827f96142e56eb9916a
OminStyle <name.cmh@gmail.com>
2013-07-11 15:09:17 -0700

    Starting to implement binary heap in solution_1.cpp

---
+++ search_backend_exercise/Solution/solution_1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class SortedList
+{
+public:
+    SortedList(T value_)
+    {
+        m_head = new Node(value_, NULL);
+        size++;
+    }
+
+    void Push(T value)
+    {
+        size++;
+        if (Size() <= 0)
+        {
+            m_head = new Node(value, NULL);
+            return;
+        }
+        Node **where = &m_head;
+        while (*where && (*where)->value < value) {
+            where = &((*where)->next);
+        }
+        *where = new Node(value,*where);
+    }
+
+    T Pop()
+    {
+        if (Size() <= 0)
+        {
+            cout << "ERROR: The SortedList is empty.";
+            return NULL;
+        }
+        Node* pResult = m_head;
+        m_head = m_head->next;
+        T result = pResult->value;
+        delete(pResult);
+        size--;
+        return result;
+    }
+
+    int Size()
+    {
+        /*
+        Node * p = m_head;
+        int size = 0;
+        while (p)
+        {
+            ++size;
+            p = p->next;
+        }*/
+        return size;
+    }
+
+private:
+    class Node
+    {
+    public: 
+        Node(T value_, Node* next_)
+        {
+            value = value_;
+            next = next_;
+        }
+        T value;
+        Node* next;
+    };
+
+    Node *m_head;
+    size = 0;
+};
+
+int main()
+    {
+        SortedList<char> list = SortedList<char>('a');
+        list.Push('b');
+        list.Pop();
+        list.Pop();
+        list.Pop();
+        list.Push('c');
+        cout << list.Size();
+        return 0;
+    }

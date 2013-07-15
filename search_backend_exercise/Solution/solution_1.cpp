10b241f601e71754039c14018b064b6a4d008811
OminStyle <name.cmh@gmail.com>
2013-07-15 01:44:27 -0700

    Implemented Push and Pop methods

--- search_backend_exercise/Solution/solution_1.cpp
+++ search_backend_exercise/Solution/solution_1.cpp
@@ -10,32 +10,17 @@ public:
     {
         array.resize(10);
         array[1] = value;
-        //m_head = new Node(value_, NULL);
         size = 1;
     }
 
     void Push(T value)
     {
-        if (size == array.size() -1)
+        if (size == array.size() - 1)
             array.resize(array.size() * 2);
         int hole = ++size;
-        for (; hole > 1 && value < array[hole/2]; hole /=2)
-            array[hole] = array[hole/2];
+        for (; hole > 1 && value < array[hole / 2]; hole /= 2)
+            array[hole] = array[hole / 2];
         array[hole] = value;
-
-        /*
-        size++;
-        if (Size() <= 0)
-        {
-            m_head = new Node(value, NULL);
-            return;
-        }
-        Node **where = &m_head;
-        while (*where && (*where)->value < value) {
-            where = &((*where)->next);
-        }
-        *where = new Node(value,*where);
-        */
     }
 
     T Pop()
@@ -45,16 +30,26 @@ public:
             cout << "ERROR: The SortedList is empty.";
             return NULL;
         }
-        array[1] = array[]
-        /*
-        Node* pResult = m_head;
-        m_head = m_head->next;
-        T result = pResult->value;
-        delete(pResult);
-        */
 
+        T min = array[1];
+        T temp = array[size - 1];
+        array[1] = temp;
+        int child;
+        int hole = 1;
+        for (; hole * 2 <= size; hole = child)
+        {
+            child = hole * 2;
+            if (child != size && array[child + 1] < array[child])
+                child++;
+            if (array[child] < temp)
+                array[hole] = array[child];
+            else
+                break;
+        }
+
+        array[hole] = temp;
         size--;
-        return result;
+        return min;
     }
 
     int Size()
@@ -63,33 +58,24 @@ public:
     }
 
 private:
-    /*
-    class Node
-    {
-    public: 
-        Node(T value_, Node* next_)
-        {
-            value = value_;
-            next = next_;
-        }
-        T value;
-        Node* next;
-    };
-
-    Node *m_head;
-    */
     int size;
     vector<T> array;
 };
 
 int main()
     {
-        SortedList<char> list = SortedList<char>('a');
-        list.Push('b');
-        //list.Pop();
-        //list.Pop();
-        //list.Pop();
-        //list.Push('c');
-        cout << list.Size();
+        SortedList<int> list = SortedList<int>(5);
+        list.Push(3);
+        list.Push(2);
+        list.Push(1);
+        list.Push(10);
+        list.Push(7);
+        cout << list.Pop() << endl;
+        cout << list.Pop() << endl;
+        cout << list.Pop() << endl;
+        cout << list.Pop() << endl;
+        cout << list.Pop() << endl;
+        cout << list.Pop() << endl;
+        //cout << list.Size() << endl;
         return 0;
     }

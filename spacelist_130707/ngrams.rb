f3187b510a70ada6d7b9ce63eda841d28e28930c
OminStyle <name.cmh@gmail.com>
2013-07-15 01:57:23 -0700

    updated ngrams description and deleted the corresponding .png file

--- spacelist_130707/ngrams.rb
+++ spacelist_130707/ngrams.rb
@@ -1,7 +1,24 @@
+# An N-gram is a sequence of N consecutive characters from a given word. For the
+# word "pilot" there are three 3-grams: "pil", "ilo" and "lot"
+# 
+# Note that your function will receive the following arguments:
+# text: which is a string containing words separated by whitespaces
+# ngramLength: which is an integer value giving the length of the n-gram
+#
+# Data contraints
+# The length of the text string will not exceed 200,000 characters
+# All words are alphanumeric (they contain only English letters a-z, A-Z and
+#	numbers 0-9)
+#
+# Efficiency contraints
+# Your function is expected to print the result in less than 2 seconds
+#
+# Example.
+# Input: text: "aaaab aOa baab c"
+# 			 ngramLength: 3
+# Output: aaa
+
 def find_ngram(text, ngram_length)
-	# Write your code here
-	# To print results to the standard output you can use puts
-	# Example puts "Hello world!"
 	pos = 0
 	frequency = Hash.new(0)
 	while pos < text.length - ngram_length
